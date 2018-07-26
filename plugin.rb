68cd8ce36ebedf69b7c04780e78d4eb1cab52f8d
boydstevens <boyd.stevens@gmail.com>
2018-07-26 12:27:41 -0400

    Enabling Write transactions
    Need to shoot myself in the foot

--- plugin.rb
+++ plugin.rb
@@ -85,7 +85,7 @@ after_initialize do
         ActiveRecord::Base.connection.transaction do
           # Setting transaction to read only prevents shoot-in-foot actions like SELECT FOR UPDATE
           # see test 'doesn't allow you to modify the database #1'
-          DB.exec "SET TRANSACTION READ ONLY"
+          #DB.exec "SET TRANSACTION READ ONLY"
           # Set a statement timeout so we can't tie up the server
           DB.exec "SET LOCAL statement_timeout = 10000"
 
